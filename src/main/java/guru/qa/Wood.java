5778c0ada758532fee1e11a647a7250916b0e7ff
Рустам <galeev560006@mail.ru>
2022-04-19 15:36:18 +0500

    wood

---
+++ src/main/java/guru/qa/Wood.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+public class Wood {
+    static int age;
+    static String Color;
+    String Treant = String.valueOf(new Wood());
+
+    public static void main (String[]args) {
+        boolean Treant = true;
+                age = 150;
+
+
+
+
+    }
+
+}
